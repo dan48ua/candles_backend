347d676f92dfa129d8e24cadf21aac4d9ee4cb4b
danyloyakovenko <151740616+dan48ua@users.noreply.github.com>
2025-04-09 19:57:30 +0000

    feat: implement createOrder method in orderService to handle order creation logic

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,5 +1,41 @@
+import prisma from 'config/prisma'
 import { IOrder } from '../dto/order.dto'
+import { basketServise } from './basket.serviсe'
 
 export class orderService {
-	public async createOrder(userId: string, orderData: IOrder) {}
+	private basketService: basketServise
+	constructor() {
+		this.basketService = new basketServise()
+	}
+
+	public async createOrder(userId: string, orderData: IOrder): Promise<IOrder> {
+		const order = await this.basketService.getBasket(userId)
+		if (!order) {
+			throw new Error('Basket not found')
+		}
+		if (order.length === 0) {
+			throw new Error('Basket is empty')
+		}
+		const totalPrice = 0
+		for (const item of order) {
+			totalPrice +- item.amount 
+		}
+		const newOrder = await prisma.order.create({
+			data: {
+				userId: userId, 
+				amount: totalPrice,
+				items: {
+					create: order.map((item) => ({
+						productId: item.product_id,
+						quantity: item.quantity,
+						amount: item.amount,
+					})),
+				}
+			}
+		})
+		if (!newOrder) {
+			throw new Error('Error creating order')
+		}
+		return newOrder
+	}
 }
