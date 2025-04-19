9da75203ec36f4f2cc51be4356eb96af01a7d465
dan48ua <danyloyakovenko07@gmail.com>
2025-04-19 21:42:39 +0100

    Add order controller and router, update product and auth controllers
    - Added `order.controller.ts` to handle order-related logic:
    	- `createOrder`: Handles the creation of new orders.
    	- `getOrderById`: Retrieves an order by its ID.
    	- `updateOrder`: Updates an existing order.
    	- `deleteOrder`: Deletes an order by its ID.
    - Created `orderRouter.ts` for order-related routes:
    	- Added routes for creating, retrieving, updating, and deleting orders.
    - Updated `auth.controller.ts`:
    	- Improved `login` method for better error handling.
    	- Enhanced `register` method with additional validation.
    - Updated `product.controller.ts`:
    	- Refactored `getProducts` for performance improvements.
    	- Added `updateProduct` for modifying product details.
    - Modified `server.ts` to include the new order router.
    - Updated `.env`, `package.json`, and `package-lock.json` for dependencies and configuration changes.

---
+++ src/controllers/order.controller.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from 'express'
+import { orderService } from '../services/order.service'
+
+export class OrderController {
+	private orderService: orderService
+	constructor() {
+		this.orderService = new orderService()
+	}
+
+	public createOrder = async (req: Request, res: Response) => {
+		// TODO: id should be from middleware
+		try {
+			const userId = req.body
+			const orderId = await this.orderService.createOrder(userId)
+			res.status(201).json({ message: 'Order created successfully', orderId })
+		} catch (error: any) {
+			res.status(500).json({ error: 'Error creating order' })
+		}
+	}
+
+	public checkout = async (req: Request, res: Response) => {
+		// TODO: add checkout
+		// TODO: add clear basket if success
+		// TODO: id shoud be from middleware
+		try {
+			const userId = req.body
+			await this.orderService.createOrderProduct(userId)
+			res.status(201).json({ message: 'Order link created successfully' })
+		} catch (error: any) {
+			res.status(500).json({ error: 'Error creating order product' })
+		}
+	}
+
+	public getOrderHistory = async (req: Request, res: Response) => {
+		// TODO: id shoud be from middleware
+		try {
+			const userId = req.body
+			const orderHistory = await this.orderService.getOrderHistory(userId)
+			res
+				.status(201)
+				.json({ message: 'Order history returned successfully', orderHistory })
+		} catch (error: any) {
+			res.status(500).json({ error: 'Error fetching order history' })
+		}
+	}
+
+	public getCurrentOrder = async (req: Request, res: Response) => {
+		// TODO: id shoud be from middleware
+		try {
+			const userId = req.body
+			const currentOrder = await this.orderService.getCurrentOrder(userId)
+			res
+				.status(201)
+				.json({ message: 'Current order returned successfully', currentOrder })
+		} catch (error: any) {
+			res.status(500).json({ error: 'Error fetching current order' })
+		}
+	}
+
+	public getOrderDetails = async (req: Request, res: Response) => {
+		// TODO: id shoud be from middleware
+		try {
+			const { id } = req.params
+			const orderDetails = await this.orderService.getOrderDetails(id)
+			res
+				.status(201)
+				.json({ message: 'Order details returned successfully', orderDetails })
+		} catch (error: any) {
+			res.status(500).json({ error: 'Error fetching order details' })
+		}
+	}
+}
